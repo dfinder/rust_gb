f36758b881aa7a5a0d93312f74b748859b4b9a04
David <davidjfinder@gmail.com>
2024-08-19 09:17:18 -0400

    Adding the joypad

--- src/joypad/mod.rs
+++ src/joypad/mod.rs
@@ -1,5 +1,54 @@
 pub mod joypad{
-    struct Mapping{
-        
+    use winit::{event::{DeviceId, KeyEvent}, keyboard::{KeyCode, PhysicalKey}};
+
+    use crate::cpu::cpu::CpuStruct;
+    enum GBKey{
+        A,B,Up,Down,Left,Right,Start,Select
+    }
+    struct KeyWrapper{
+        v_key: GBKey,
+        p_key: KeyCode,
+        state: bool,
+        //time: u32
+    }
+    pub struct Joypad{
+        mapping:[KeyWrapper;8]
+    }
+    impl Joypad {
+        pub fn new(map:[KeyCode;8]) -> Self{
+            let a:KeyWrapper = KeyWrapper{v_key: GBKey::A,p_key:map[0],state:false};
+            let b:KeyWrapper = KeyWrapper{v_key: GBKey::B,p_key:map[1],state:false};
+            let up:KeyWrapper = KeyWrapper{v_key: GBKey::Up,p_key:map[2],state:false};
+            let down:KeyWrapper =KeyWrapper{v_key: GBKey::Down,p_key:map[3],state:false};
+            let left:KeyWrapper = KeyWrapper{v_key: GBKey::Left,p_key:map[4],state:false};
+            let right:KeyWrapper = KeyWrapper{v_key: GBKey::Right,p_key:map[5],state:false}; 
+            let start:KeyWrapper = KeyWrapper{v_key: GBKey::Start,p_key:map[6],state:false}; 
+            let select:KeyWrapper = KeyWrapper{v_key: GBKey::Select,p_key:map[7],state:false};
+            return Joypad{
+
+                mapping:[a,b,up,down,left,right,start,select]
+
+            }
+        }
+        pub fn process_keystrokes(&mut self, d_id: DeviceId, key_event: KeyEvent, synthetic: bool){
+          for (index,key_wrapper ) in self.mapping.iter().enumerate(){
+            //match key_event
+            if synthetic == false && key_event.repeat==false {
+                match key_event.physical_key{
+                    PhysicalKey::Code(key_press) => {
+                        if key_press == key_wrapper.p_key{
+                            key_wrapper.state = !key_event.state.is_pressed();
+                        }
+                    }
+                    PhysicalKey::Unidentified(_) => todo!(),
+                }
+            }
+          }
+        }
+        pub fn set_key_stroke_nibble(&mut self, current_val:u8,)->u8{
+            
+
+        }
+        //fn hit_list()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ pub mod screen;
 mod cpu_state;
 mod memory_wrapper;
 pub mod function_table;
-use winit::event_loop::EventLoopBuilder;
+use glutin::api::egl::device;
+use joypad::joypad::{self, Joypad};
+use winit::{event_loop::EventLoopBuilder, keyboard::{Key, KeyCode}};
 use cpu::cpu::CpuStruct;
 use glium::*;
 
 use crate::screen::screen::display_screen;
-mod joypad;
+pub mod joypad;
 #[macro_use]
 pub mod cpu;
 pub mod registers;
@@ -15,19 +17,26 @@ pub mod memory;
 extern crate glium;
 fn main() {
     let mut my_cpu = cpu::cpu::CpuStruct::new();
-    let mut graphics_state:&[u8;8192];
+    let mut graphics_state:&[u8;8192]; //This doesn't actually have to be a borrow
     let event_loop = EventLoopBuilder::new().build().expect("event loop building");
     let (_window, display) = glium::backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
     let mut frame = display.draw();
+    let mut joypad:Joypad = joypad::joypad::Joypad::new([KeyCode::KeyZ, KeyCode::KeyX, KeyCode::ArrowUp,KeyCode::ArrowDown,KeyCode::ArrowLeft,KeyCode::ArrowRight,KeyCode::KeyM, KeyCode::KeyN]);
     loop{
         my_cpu.interpret_command();
         graphics_state = my_cpu.fetch_graphics();
+        
         let _ = event_loop.run(move |event, window_target| {
             match event {
                     winit::event::Event::WindowEvent { event, .. } => match event {
-                    winit::event::WindowEvent::CloseRequested => window_target.exit(),
-                    _ => (),
-                },
+                        winit::event::WindowEvent::CloseRequested => window_target.exit(),
+                        winit::event::WindowEvent::KeyboardInput { device_id, event, is_synthetic } => {
+                            joypad.process_keystrokes(&mut &my_cpu,device_id,event,is_synthetic);
+                        }
+                    
+                        _ => (),
+                     },
+                     
                 _ => (),
             };}
          );
